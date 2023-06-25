4cbb408d0101ef590b1bcbaa254435121ad8498a
Gouvernon Stan <stan.gouvernon@rpn.ch>
2023-06-25 14:46:26 +0200

    New structure for unit tests

---
+++ Impero/UnitTestLibrary/UnitTestStudent.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryData;
+using System.Collections.Generic;
+
+namespace UnitTestStudent
+{
+    
+}
--- Impero/UnitTestLibrary/UnitTestUrlClass.cs
+++ Impero/UnitTestLibrary/UnitTestUrlClass.cs
@@ -4,7 +4,7 @@ using LibraryData;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace UnitTestUrlClass
+namespace UnitTestClassLibrary
 {
     [TestClass]
     public class UnitTestUrl
@@ -42,6 +42,20 @@ namespace UnitTestUrlClass
                 TestHistory.AddUrl(new Url(DateTime.Now, browser.Key, "testurl"));
                 Assert.AreEqual(TestHistory.AllBrowser[browser.Key].Count, 1);
             }
+        }
+    }
+
+    [TestClass]
+    public class UnitTestStreamOptions
+    {
+        [TestMethod]
+        public void StreamOptionsConstructor()
+        {
+            Priority priority = Priority.Topmost;
+            List<string> strings = new List<string> { "test1", "test2", "test3" };
+            StreamOptions options = new StreamOptions(priority,strings);
+            Assert.AreEqual(options.priority, priority);
+            Assert.AreEqual(options.focus, strings);
         }
     }
 }
