6d11b4e9dd89d8ce837775f2e6984836e9c2122b
Gouvernon Stan <stan.gouvernon@rpn.ch>
2023-08-28 20:50:56 +0200

    Trying to create a browser integrated in the ApplicationStudent to replace selenium.

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WebBrowser
+{
+    partial class Browser
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.tbxUrl = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnMoveBack = new System.Windows.Forms.Button();
+            this.btnMoveForward = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(0, 54);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(800, 396);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            this.webView.SourceChanged += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2SourceChangedEventArgs>(this.webView_SourceChanged);
+            // 
+            // tbxUrl
+            // 
+            this.tbxUrl.Location = new System.Drawing.Point(141, 12);
+            this.tbxUrl.Name = "tbxUrl";
+            this.tbxUrl.Size = new System.Drawing.Size(365, 20);
+            this.tbxUrl.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(512, 10);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "button1";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnMoveBack
+            // 
+            this.btnMoveBack.Location = new System.Drawing.Point(12, 10);
+            this.btnMoveBack.Name = "btnMoveBack";
+            this.btnMoveBack.Size = new System.Drawing.Size(24, 23);
+            this.btnMoveBack.TabIndex = 3;
+            this.btnMoveBack.Text = "<-";
+            this.btnMoveBack.UseVisualStyleBackColor = true;
+            this.btnMoveBack.Click += new System.EventHandler(this.btnMoveBack_Click);
+            // 
+            // btnMoveForward
+            // 
+            this.btnMoveForward.Location = new System.Drawing.Point(42, 9);
+            this.btnMoveForward.Name = "btnMoveForward";
+            this.btnMoveForward.Size = new System.Drawing.Size(24, 23);
+            this.btnMoveForward.TabIndex = 4;
+            this.btnMoveForward.Text = "->";
+            this.btnMoveForward.UseVisualStyleBackColor = true;
+            this.btnMoveForward.Click += new System.EventHandler(this.btnMoveForward_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(72, 9);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(24, 23);
+            this.btnReload.TabIndex = 5;
+            this.btnReload.Text = "O";
+            this.btnReload.UseVisualStyleBackColor = true;
+            // 
+            // Browser
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.btnMoveForward);
+            this.Controls.Add(this.btnMoveBack);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbxUrl);
+            this.Controls.Add(this.webView);
+            this.Name = "Browser";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.TextBox tbxUrl;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnMoveBack;
+        private System.Windows.Forms.Button btnMoveForward;
+        private System.Windows.Forms.Button btnReload;
+    }
+}
+
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Browser : Form
+    {
+        public Browser(List<string> autorisedWebsites)
+        {
+            InitializeComponent();
+            AutorisedWebsites = autorisedWebsites;
+            visitedUrls.Add(new Uri("https://duckduckgo.com/"));
+        }
+
+        public Browser()
+        {
+            InitializeComponent();
+            visitedUrls.Add(new Uri("https://duckduckgo.com/"));
+        }
+
+        private List<string> AutorisedWebsites = new List<string>();
+        private List<Uri> visitedUrls = new List<Uri>();
+        private int currentUrlId = 0;
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            while (currentUrlId < visitedUrls.Count)
+            {
+                visitedUrls.RemoveAt(visitedUrls.Count - 1);
+            }
+            try
+            {
+                webView.Source = new Uri(tbxUrl.Text);
+                visitedUrls.Add(new Uri(tbxUrl.Text));
+
+            }
+            catch
+            {
+                webView.Source = new Uri("https://duckduckgo.com/?t=ffab&q="+tbxUrl.Text+"&atb=v320-1&ia=web");
+                visitedUrls.Add(new Uri("https://duckduckgo.com/?t=ffab&q=" + tbxUrl.Text + "&atb=v320-1&ia=web"));
+            }
+            currentUrlId++;
+        }
+
+        private void webView_SourceChanged(object sender, Microsoft.Web.WebView2.Core.CoreWebView2SourceChangedEventArgs e)
+        {
+            tbxUrl.Text = webView.Source.ToString();
+        }
+
+        private void btnMoveBack_Click(object sender, EventArgs e)
+        {
+            if(currentUrlId== 0) { return; }
+            currentUrlId--;
+            webView.Source = visitedUrls[currentUrlId];
+        }
+
+        private void btnMoveForward_Click(object sender, EventArgs e)
+        {
+            if(currentUrlId == visitedUrls.Count) { return; }
+            currentUrlId++;
+            webView.Source = visitedUrls[currentUrlId];
+        }
+    }
+}
