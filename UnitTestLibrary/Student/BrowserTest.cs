0f0f9ef50c4ba9df161dfd140c4c8dc60c91f7cf
Gouvernon <stan@gouvernon.org>
2023-09-04 16:33:13 +0200

    Created the file to test the browser class

---
+++ UnitTestLibrary/Student/BrowserTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestLibrary.Student
+{
+    [TestClass]
+    public class BrowserTest
+    {
+        [TestMethod]
+        public void Constructors() { }
+    }
+
+    [TestClass]
+    public class TabTest
+    {
+        [TestMethod]
+        public void Constructors() { }
+    }
+
+    [TestClass]
+    public class ControlBarTest
+    {
+        [TestMethod]
+        public void Constructors() { }
+    }
+
+    [TestClass]
+    public class TabPreviewTest
+    {
+        [TestMethod]
+        public void Constructors() { }
+    }
+
+    [TestClass]
+    public class CustomWebView2Test
+    {
+        [TestMethod]
+        public void Constructors() { }
+    }
+}
