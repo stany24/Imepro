fe7b2cea9d913119035526ac85212be9e406c65c
Gouvernon <stan@gouvernon.org>
2023-10-24 08:45:15 +0200

    Created the project for the student application with avalonia.

---
+++ StudentSoftware/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace StudentSoftware.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ StudentSoftware/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace StudentSoftware.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
